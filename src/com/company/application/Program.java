6eb6eeec7a61cf8ec7225ebb35c140b3c4fd45f4
auricio52 <auricio52@gmail.com>
2022-10-03 22:26:05 -0300

    Starting to implement Board and Piece

--- src/com/company/application/Program.java
+++ src/com/company/application/Program.java
@@ -1,11 +1,12 @@
 package com.company.application;
 
+import com.company.boardgame.Board;
 import com.company.boardgame.Position;
 
 public class Program {
     public static void main(String[] args) {
-        Position position = new Position(1, 2);
+        Board board = new Board(8,8);
 
-        System.out.println(position);
+        System.out.println(board);
     }
 }
