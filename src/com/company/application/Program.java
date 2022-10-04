5ed65e9a6901c09acb3bd6e2d5741245f977712b
auricio52 <auricio52@gmail.com>
2022-10-03 22:15:57 -0300

    First class: Position

--- src/com/company/application/Program.java
+++ src/com/company/application/Program.java
@@ -1,7 +1,11 @@
 package com.company.application;
 
+import com.company.boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        
+        Position position = new Position(1, 2);
+
+        System.out.println(position);
     }
 }
